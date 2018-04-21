6fd84d8204346342b3868ae71113a86f8620a89c
Miles Zimmerman <mileszim@protonmail.com>
2018-04-20 20:22:28 -0700

    Bootstrap and stuff

--- config/initializers/content-security-policy.js
+++ config/initializers/content-security-policy.js
@@ -2,6 +2,16 @@ module.exports = function(environment) {
   return {
     'script-src': "'self' 'unsafe-eval' apis.google.com",
     'frame-src': "'self' https://*.firebaseapp.com",
-    'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com",
+    'font-src': [
+      "'self'",
+      "https://fonts.gstatic.com",
+      "data:"
+    ],
+    'style-src': [
+      "'self'",
+      "'unsafe-inline'",
+      "https://fonts.googleapis.com"
+    ]
   };
 };
